44a234b68fe049aa606a9cdd550f8effb1b85ecc
Doroshenko Oleksandr <doroshenkoaldm@gmail.com>
2026-09-21 10:50:26 +0300

    feat: add yin-yang OD favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ''
+
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="64x64" href={`${basePath}/favicon.png`} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
